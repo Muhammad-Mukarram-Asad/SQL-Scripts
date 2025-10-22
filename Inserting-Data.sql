3289d64a1b3f2d2c86efe38b4f8a9f6124d0a627
Mukarram Asad <76425569+Muhammad-Mukarram-Asad@users.noreply.github.com>
2025-10-22 15:14:06 +0500

    Update Inserting-Data.sql

--- Inserting-Data.sql
+++ Inserting-Data.sql
@@ -1,2 +1,7 @@
 use users;
-Insert values() into users ();
+-- Insert values in the users table:
+INSERT into users (name, email,gender, date_of_birth) values
+("Abrar", "avrar@gmail.com","Male","1990-01-15");
+("babar", "avrar@gmail.com","Male","1990-01-15"),
+("Cat", "avrar@gmail.com","Male","1990-01-15"),
+("Daniel", "avrar@gmail.com","Male","1990-01-15");
