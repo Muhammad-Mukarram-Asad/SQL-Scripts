8c9226e413e7d3941f0f6ef3f42a90d602c4b44e
Mukarram Asad <76425569+Muhammad-Mukarram-Asad@users.noreply.github.com>
2025-10-22 15:13:44 +0500

    Update create-database.sql

--- create-database.sql
+++ create-database.sql
@@ -10,12 +10,5 @@ create table users (
   createdAt default timestamp
   );
 
--- Insert values in the users table:
-INSERT into users (name, email,gender, date_of_birth) values
-("Abrar", "avrar@gmail.com","Male","1990-01-15");
-("babar", "avrar@gmail.com","Male","1990-01-15"),
-("Cat", "avrar@gmail.com","Male","1990-01-15"),
-("Daniel", "avrar@gmail.com","Male","1990-01-15");
-
 -- For Quering the table
 select * from users;
