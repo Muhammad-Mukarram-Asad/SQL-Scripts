38ce72410bf41a0fc5ac5995cd1c639fe95dc363
Mukarram Asad <76425569+Muhammad-Mukarram-Asad@users.noreply.github.com>
2025-11-07 22:57:16 +0500

    Add files via upload

---
+++ adding dummy data.sql
@@ -0,0 +1,21 @@
+-- This sql query is useful for quickly adding dummy data into sql database:
+
+-- INSERT INTO users (name, email, gender, date_of_birth,salary, is_Active) VALUES
+-- ('Mukarram', 'mukii@example.com', 'Male', '2001-08-15', 86000, true),
+-- ('Alina', 'alina@example.com', 'Female', '2002-04-20', 87000, true),
+-- ('Shaheer', 'shaheer@example.com', 'Male', '2003-04-30', 88000, true),
+-- ('Amna', 'amna@example.com', 'Female', '2006-10-10', 89000, true),
+-- ("Arham", "arham@gmail.com", "Male", "2001-08-16", 90000, true),
+-- ("Babar", "babar@gmail.com", "Male", "2001-08-16", 91000, true),
+-- ("Syed", "syed@gmail.com", "Male", "2001-08-16", 92000, true),
+-- ('shaheena', 'shaheena@yahoo.com',"Female", "1962-01-01", 93000, true),
+-- ('Maham', 'maham@yahoo.com',"Female", "2001-08-06", 93000, true),
+-- ('Shafique', 'shafique@hotmail.com',"Male", "2000-01-01", 93000, true),
+-- ('Rizwan', 'rizwan@yahoo.com',"Male", "1996-01-01", 93000, true),
+-- ('shaheen', 'shaheen@yahoo.com',"Male", "1998-01-01", 93000, true),
+-- ('Haris', 'haris@yahoo.com',"Male", "1999-10-10", 93000, true);
+-- update users set is_Active = false where id = 11;
+select * from users;
+
+
+
